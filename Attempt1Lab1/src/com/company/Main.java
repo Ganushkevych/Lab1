e4067a76717784d5935e998935bfad5349c8f94f
Ganushkevych <72021081+Ganushkevych@users.noreply.github.com>
2023-04-02 19:14:19 +0300

    Delete Attempt1Lab1 directory

--- Attempt1Lab1/src/com/company/Main.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println(StringCalculator.add("// \n-1    2  3"));
-    }
-}
